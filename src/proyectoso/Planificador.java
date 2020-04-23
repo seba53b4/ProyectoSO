f5afd95610b2cc3cd5e71e2649eb0cc0076f014d
seba53b4 <seba53b4@hotmail.com>
2020-04-23 19:14:31 -0300

    Initial commit

---
+++ src/proyectoso/Planificador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoso;
+
+/**
+ * Clase encargada de mantener el orden de ejecucion de los hilos
+ * @author Seba-OS
+ */
+public class Planificador {
+    
+}
---
+++ src/proyectoso/Reloj.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoso;
+
+/**
+ * Encagada de simular el tiempo
+ * @author Seba-OS
+ */
+public class Reloj {
+    
+}
